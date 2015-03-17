4d58b7fa2880d49572fb2e12f2b54e72612537f0
Nacho <ildb.88@gmail.com>
2015-03-17 17:38:54 -0500

    It works as it should

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object
+## that stores a numeric invertible matrix and cache's its inverse
 
-## Write a short comment describing this function
+## creates a special object, which is really a list containing functions
+# 1.- set the value of the matrix
+# 2.- get the value of the matrix
+# 3.- set the value of the inverse
+# 4.- get the value of the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(solve) m <<- solve
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the object-function created with the above function. 
+## However, it first checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the data and sets the inverse 
+## in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
